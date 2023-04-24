2e747766cdd6e313e060b18af2c6282ff78058a6
DE Juniada <dejon933@gmail.com>
2023-04-24 22:36:28 +0800

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,15 @@
+<?php
+$url = $_GET['url'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+curl_close($ch);
+
+header('Content-Type: application/json');
+echo $response;
+?>
